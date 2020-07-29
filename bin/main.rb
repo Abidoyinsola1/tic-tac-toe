f35487fbbe7c46b1321922b308d11a1a37e33f97
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-29 16:23:29 +0300

    Add board layout

--- bin/main.rb
+++ bin/main.rb
@@ -4,7 +4,12 @@ player1 = gets.chomp
 puts "Enter your name: "
 player2 = gets.chomp
 
-
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+puts "#{board[0]} | #{board[1]} | #{board[2]}"
+puts "---------" 
+puts "#{board[3]} | #{board[4]} | #{board[5]}"
+puts "---------"
+puts "#{board[6]} | #{board[7]} | #{board[8]}"
 
 puts "#{player1} pick a number from 1-9"
 move1 = gets.chomp
