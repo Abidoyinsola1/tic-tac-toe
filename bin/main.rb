6e03628031fc78801de43b92077c13bbe5a80a58
abidoyinsola1 <ifeobayemi@gmail.com>
2020-07-29 13:57:52 +0100

    display player's inputs

--- bin/main.rb
+++ bin/main.rb
@@ -3,3 +3,10 @@ puts "Enter your name: "
 player1 = gets.chomp
 puts "Enter your name: "
 player2 = gets.chomp
+
+
+
+puts "#{player1} pick a number from 1-9"
+move1 = gets.chomp
+puts "#{player2} pick a number from 1-9"
+move2 = gets.chomp
