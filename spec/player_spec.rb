770a587bbbc4576dc80c47bb82f3e2cc8cffe111
Joseph Burke <joseph_burke@outlook.com>
2020-08-14 09:25:19 +0100

    Add positive and negative tests for Player class #add_pos method

---
+++ spec/player_spec.rb
@@ -0,0 +1,17 @@
+require 'player'
+
+describe Player do
+  describe '#add_pos' do
+    it "pushes a position to the player's @position instance variable" do
+      player = Player.new('Joe', 'X')
+      player.add_pos(3)
+      expect(player.position).to eql([3])
+    end
+
+    it 'does not leave the @position instance variable as an empty array' do
+      player = Player.new('Joe', 'X')
+      player.add_pos(3)
+      expect(player.position.empty?).not_to eql(true)
+    end
+  end
+end
